6ebfa95d065b2dd5b490d81293944f75ba8fcd01
Juliana Arroyave Arango <julianaarroyave301009@correo.itm.edu.co>
2026-08-24 19:47:08 -0500

    Refactoriza script aplicando PEP 8 y type hints

--- ejercicios-clase/semana-02/refactor_pep8.py
+++ ejercicios-clase/semana-02/refactor_pep8.py
@@ -1,8 +1,24 @@
-def CalcularPromedio(Lista):
-    s=0
-    for x in Lista:
-     s=s+x
-    return s/len(Lista)
- 
-l=[1,2,3,4,5]
-print(CalcularPromedio(l))
+def calcular_promedio(lista: list[float]) -> float:
+    """
+    Calcula el promedio (media aritmética) de una lista de números.
+
+    Parámetros:
+        lista (list[float]): Lista de números (enteros o flotantes)
+            de la cual se calculará el promedio.
+
+    Retorna:
+        float: El promedio de los elementos de la lista.
+    """
+    suma = 0
+    for x in lista:
+        suma = suma + x
+    return suma / len(lista)
+
+
+def main() -> None:
+    lista = [1, 2, 3, 4, 5]
+    print(calcular_promedio(lista))
+
+
+if __name__ == "__main__":
+    main()
